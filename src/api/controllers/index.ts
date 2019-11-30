d7e2b0b9ba12e445052c169b19ba43de8e954587
akinduko <akindukooa@gmail.com>
2019-11-30 16:48:25 +0100

    updated: removed rest structure

--- src/api/controllers/index.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from 'express';
-
-export default {
-    home: (req: Request, res: Response): void => {
-        res.render('index.ejs', { title: 'Ginjabox  Middleware Engine' });
-    }
-};
--- src/api/integrations/AuthService/index.ts
+++
@@ -1,9 +0,0 @@
-import Auth from './auth';
-
-export default class AuthService {
-    public auth: Auth;
-
-    public constructor() {
-        this.auth = new Auth();
-    }
-}
