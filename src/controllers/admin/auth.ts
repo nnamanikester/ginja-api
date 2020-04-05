d6a4046b1aa528a86f93ca312b04c4ee7b096133
John Kester <nnamanikester@gmail.com>
2020-04-05 17:54:00 +0100

    Authentication api working correctly!

--- src/controllers/admin/auth.ts
+++ src/controllers/admin/auth.ts
@@ -1,10 +1,34 @@
 import { Request, Response, NextFunction } from 'express';
+import { prisma } from '../../core/prisma/generated';
 
 const authController: any = async (req: Request, res: Response, next: NextFunction): Promise<any> => {
     try {
-        res.json({ message: 'Yes' });
+        const [staff]: any[] = await prisma.adminUsers({
+            where: {
+                email: req.body.email
+            }
+        });
+        if (!staff) {
+            return res.status(404).json({
+                success: false,
+                error: true,
+                message: `There's no staff with the email  ${req.body.email}`
+            });
+        }
+        if (staff.password === req.body.password) {
+            return res.json({
+                success: true,
+                error: false,
+                message: 'Authentication Successful!'
+            });
+        }
+        return res.status(400).json({
+            success: false,
+            error: true,
+            message: 'Wrong Password!'
+        });
     } catch (error) {
-        next(error);
+        return next(error);
     }
 };
 
